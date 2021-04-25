e5f9ce007a67c24cb7166bef5de62a530fc7af65
zhangtengfei <zhangtengfei@pwrd.com>
2021-04-25 11:57:16 +0800

    crsf防范 - 前后端不分离 - 未成功

--- security-03-csrf/src/main/java/com/springboot/security/controller/HelloController.java
+++ security-03-csrf/src/main/java/com/springboot/security/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.springboot.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,5 +27,15 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    /**
+     * 这个测试接口是一个 POST 请求，因为默认情况下，GET、HEAD、TRACE 以及 OPTIONS 是不需要验证 CSRF 攻击的。
+     * 
+     * @return
+     */
+    @PostMapping("/hello")
+    public String csrf() {
+        return "csrf hello";
     }
 }
